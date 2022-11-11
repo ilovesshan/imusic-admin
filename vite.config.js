410b892f78575e00f3dcc7b031ec61fce1e9f189
wangyifeng <2665939276@qq.com>
2022-11-11 16:11:32 +0800

    update: port and cpu info

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    port: 8080,
-  },
+  // server:{
+  //   port: 8080,
+  // },
   plugins:
     [vue({
       template: transformAssetUrls
