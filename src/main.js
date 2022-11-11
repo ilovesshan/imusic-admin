5515ab705d8db03b57c3430ec5aa6667b02a68dc
wangyifeng <2665939276@qq.com>
2022-11-11 15:43:44 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+
+import './style.css'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
+
+app.mount('#app')
