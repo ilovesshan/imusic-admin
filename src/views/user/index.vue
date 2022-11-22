f4ea02366a3753709b57db6f6dac50dc74dbf6c7
wangyifeng <2665939276@qq.com>
2022-11-22 12:39:45 +0800

    feat：用户新增功能

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="user-page">
+
+    <!-- 搜索框 -->
     <div class="row search-bar">
 
       <div class="col-2">
@@ -25,14 +27,40 @@
       </div>
 
       <div class="col-2 btns">
+        <q-btn outline @click="addUser" color="secondary" icon="add" label="新增" />
         <q-btn outline @click="loadTata" color="primary" icon="search" label="搜索" />
-        <q-btn outline @click="reset" color="secondary" icon="restart_alt" label="重置" />
+        <q-btn outline @click="reset" color="amber" icon="restart_alt" label="重置" />
       </div>
     </div>
+
+    <!-- 表格 -->
     <q-table :rows="rows" :columns="columns" row-key="name" hide-pagination />
     <div class="pagination">
       <q-pagination boundary-numbers v-model="selectConditions.pageNum" :max="total" max-pages="6" direction-links />
     </div>
+
+
+    <!-- 新增用户对话框 -->
+    <q-dialog v-model="addUserDialog">
+      <q-card style="width:30%;">
+        <q-card-section>
+          <div class="text-h6">新增用户</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input filled v-model="username" :dense="false" label="用户名" />
+          <br />
+          <q-input filled v-model="password" type="password" :dense="false" label="密码" />
+          <br />
+          <q-input filled v-model="confirmPassword" type="password" :dense="false" label="确认密码" />
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="确定" color="primary" @click="submitAddUser" />
+          <q-btn flat label="取消" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
@@ -41,12 +69,17 @@
 import type { ISelectConditions, IUserData } from './type';
 import { useTableColumn } from "./hooks"
 import { reactive, ref, watch } from 'vue';
-import { getUserList } from '@/api/user';
+import { getUserList, insertUser } from '@/api/user';
+import { showError, showSuccess } from '@/utils/Notify';
+
+let addUserDialog = ref<boolean>(false);
+const total = ref<number>(0);
+const username = ref<string>("");
+const password = ref<string>("");
+const confirmPassword = ref<string>("");
 
 const columns = useTableColumn();
 const rows = ref<Array<IUserData>>([])
-
-const total = ref<number>(0);
 let selectConditions = reactive<ISelectConditions>({
   pageNum: 1,
   pageSize: 5,
@@ -90,6 +123,31 @@ const reset = () => {
   selectConditions.gender = null;
   selectConditions = { pageNum: 1, pageSize: 3 };
   loadTata();
+}
+
+const submitAddUser = () => {
+
+  if (confirmPassword.value !== password.value) {
+    showError("两次密码输入不一致");
+    return;
+  }
+
+  insertUser(username.value, password.value).then(res => {
+    const { code, data, message } = res;
+    if (code === 200) {
+      showSuccess(message);
+      loadTata();
+      addUserDialog.value = false;
+    } else {
+      showError(message);
+    }
+  })
+}
+
+const addUser = () => {
+  username.value = "";
+  password.value = "";
+  addUserDialog.value = true;
 }
 
 watch(() => selectConditions.pageNum, (_) => {
